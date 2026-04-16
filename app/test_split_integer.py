4d67cc925e35c59314766758d87b2e922c46b620
Alisa Hoys <hoys@protonmail.com>
2026-04-16 17:15:57 +0200

    Corrected Implementation of tests and function

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -5,6 +5,7 @@ def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     value = 12
     number_of_parts = 4
     result = split_integer(value, number_of_parts)
+    assert len(result) == number_of_parts
     assert sum(result) == value
 
 
@@ -20,13 +21,18 @@ def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     number_of_parts = 1
     result = split_integer(value, number_of_parts)
     assert result == [12]
+    assert len(result) == 1
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    value = 10
-    number_of_parts = 3
-    result = split_integer(value, number_of_parts)
+    result = split_integer(7, 3)
+    assert result == [2, 2, 3]
+
+    result = split_integer(10, 3)
     assert result == [3, 3, 4]
+
+    result = split_integer(8, 3)
+    assert result == [2, 3, 3]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
@@ -34,3 +40,4 @@ def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
     number_of_parts = 4
     result = split_integer(value, number_of_parts)
     assert result == [0, 0, 0, 0]
+    assert sum(result) == 0
